f2fe21ed1dddfedffc0b5ab7d46193d1c4530b1f
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-11 13:44:42 -0400

    Add logInformation logic

--- app/src/main/java/com/example/sqlitecursoradapter/MainActivity.java
+++ app/src/main/java/com/example/sqlitecursoradapter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sqlitecursoradapter;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -40,7 +41,31 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void loadInformation(View view) {
+    public void logInformation(View view) {
+        UsersDatabaseHelper usersDatabaseHelper = new UsersDatabaseHelper(getApplicationContext());
 
+        final String POSTS_SELECT_QUERY = "SELECT * FROM users";
+
+        SQLiteDatabase db = usersDatabaseHelper.getWritableDatabase();
+        Cursor cursor = db.rawQuery(POSTS_SELECT_QUERY, null);
+
+        try {
+            if (cursor.moveToFirst()) {
+                do {
+                    String nameUser = cursor.getString(cursor.getColumnIndex(UsersDatabaseHelper.KEY_USER_NAME));
+                    String ageUser = cursor.getString(cursor.getColumnIndex(UsersDatabaseHelper.KEY_USER_ID));
+
+                    Log.d(TAG, "logInformation: " + nameUser);
+                    Log.d(TAG, "logInformation: " + ageUser);
+
+                } while (cursor.moveToNext());
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "Error while trying to get posts from database");
+        } finally {
+            if (cursor != null && !cursor.isClosed()) {
+                cursor.close();
+            }
+        }
     }
 }
